74ec8156848907c33c4aa5faf697db8959bc9948
pdanna632 <pdanna632@gmail.com>
2026-07-14 14:02:16 -0500

    Update homework.py

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,11 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import zipfile
+import glob
+import pandas as pd
+import os
+from datetime import datetime
 
 def clean_campaign_data():
     """
@@ -49,6 +54,52 @@ def clean_campaign_data():
 
 
     """
+
+    zip_folder = 'files/input/*'
+
+    dataframes = []
+
+    for zip_path in glob.glob(zip_folder):
+        with zipfile.ZipFile(zip_path, 'r') as z:
+            csv_filename = z.namelist()[0]
+            with z.open(csv_filename) as csv_file:
+                df = pd.read_csv(csv_file)
+                dataframes.append(df)
+
+    combined_df = pd.concat(dataframes, ignore_index=True)
+
+    client_df = pd.DataFrame()
+
+    client_df["client_id"] = combined_df["client_id"]
+    client_df["age"] = combined_df["age"]
+    client_df["job"] = combined_df["job"].str.replace(".", "").str.replace("-", "_")
+    client_df["marital"] = combined_df["marital"]
+    client_df["education"] = combined_df["education"].str.replace(".", "_").str.replace("unknown","pd.NA")
+    client_df["credit_default"] = combined_df["credit_default"].apply(lambda x: x.replace("yes", "1") if "yes" in x else "0")
+    client_df["mortgage"] = combined_df["mortgage"].apply(lambda x: x.replace("yes", "1") if "yes" in x else "0")
+
+    campaign_df = pd.DataFrame()
+
+    campaign_df["client_id"] = combined_df["client_id"]
+    campaign_df["number_contacts"] = combined_df["number_contacts"]
+    campaign_df["contact_duration"] = combined_df["contact_duration"]
+    campaign_df["previous_campaign_contacts"] = combined_df["previous_campaign_contacts"]
+    campaign_df["previous_outcome"] = combined_df["previous_outcome"].apply(lambda x: x.replace("success", "1") if "success" in x else "0")
+    campaign_df["campaign_outcome"] = combined_df["campaign_outcome"].apply(lambda x: x.replace("yes", "1") if "yes" in x else "0")
+    campaign_df["last_contact_date"] = combined_df.apply(lambda row: datetime.strptime(f"2022-{row['month']}-{row['day']}", "%Y-%b-%d").strftime("%Y-%m-%d"), axis=1)
+    economics_df = pd.DataFrame()
+
+    economics_df["client_id"] = combined_df["client_id"]
+    economics_df["cons_price_idx"] = combined_df["cons_price_idx"]
+    economics_df["euribor_three_months"] = combined_df["euribor_three_months"]
+
+    output_folder = "files/output"
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    client_df.to_csv(os.path.join(output_folder, 'client.csv'), index=False)
+    campaign_df.to_csv(os.path.join(output_folder, 'campaign.csv'), index=False)
+    economics_df.to_csv(os.path.join(output_folder, 'economics.csv'), index=False)
 
     return
 
